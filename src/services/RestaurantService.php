ceb6935372dd1d7e9327667059119d44ee39c257
Miłosz Gilga <business@miloszgilga.pl>
2022-11-30 14:50:52 +0100

    add sample return arrays and main banner error

--- src/services/RestaurantService.php
+++ src/services/RestaurantService.php
@@ -9,8 +9,8 @@
  * Data utworzenia: 2022-11-27, 20:00:52                       *
  * Autor: cptn3m012                                            *
  *                                                             *
- * Ostatnia modyfikacja: 2022-11-27 20:49:38                   *
- * Modyfikowany przez: cptn3m012                               *
+ * Ostatnia modyfikacja: 2022-11-30 14:38:55                   *
+ * Modyfikowany przez: Miłosz Gilga                            *
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 namespace App\Services;
@@ -19,6 +19,8 @@ use App\Core\MvcService;
 
 class RestaurantService extends MvcService
 {
+    private $_error;
+
     protected function __construct()
     {
         parent::__construct();
@@ -26,11 +28,31 @@ class RestaurantService extends MvcService
 
     public function add_restaurant()
     {
-       
+        $this->_error = 'To jest przykładowy error z add restaurant';
+        // tak samo jak w authservice (w sensie wartości zwrócone w postaci array('value' => '', 'invl' => false, 'bts_class' => 'is-valid'))
+        return array(
+            'v_name' => '',
+            'v_price' => '',
+            'v_street' => '',
+            'v_building_no' => '',
+            'v_post_code' => '',
+            'v_city' => '',
+            'error' => $this->_error,
+        );
     }
 
     public function edit_restaurant()
     {
-       
+        $this->_error = 'To jest przykładowy error z edit restaurant';
+        // tak samo jak w authservice (w sensie wartości zwrócone w postaci array('value' => '', 'invl' => false, 'bts_class' => 'is-valid'))
+        return array(
+            'v_name' => '',
+            'v_price' => '',
+            'v_street' => '',
+            'v_building_no' => '',
+            'v_post_code' => '',
+            'v_city' => '',
+            'error' => $this->_error,
+        );
     }
 }
