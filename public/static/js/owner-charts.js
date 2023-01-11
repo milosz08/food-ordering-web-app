b5664dd58e5c2e397d06bef31b8a8087a59cff08
patrick012016 <patryk012016@gmail.com>
2023-01-11 22:52:28 +0100

    Added support for the chart in the restaurateur panel

--- public/static/js/owner-charts.js
+++ public/static/js/owner-charts.js
@@ -7,15 +7,23 @@
  * Data utworzenia: 2023-01-06, 04:16:09                       *
  * Autor: Miłosz Gilga                                         *
  *                                                             *
- * Ostatnia modyfikacja: 2023-01-06 04:51:47                   *
- * Modyfikowany przez: Miłosz Gilga                            *
+ * Ostatnia modyfikacja: 2023-01-11 21:13:56                   *
+ * Modyfikowany przez: patrick012016                           *
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 function generateOwnerCharData() {
-    const xValues = [ "Poniedziałek", "Wtorek", "Środa", "Czwartek", "Piątek", "Sobota" ];
-    const yValues = [ 30, 22, 43, 21, 25, 30 ];
+    var dataDayName = [];
+    const options = { weekday: 'long' };
+    for (let i = 0; i < 6; i++) 
+    {
+        const input = new Date(jArray[i]["Day"]);
+        dataDayName[i] = input.toLocaleDateString(undefined, options);
+    }
+
+    const xValues = [ dataDayName[5], dataDayName[4], dataDayName[3], dataDayName[2], dataDayName[1], dataDayName[0] ];
+    const yValues = [ jArray[5]["Amount"], jArray[4]["Amount"], jArray[3]["Amount"], jArray[2]["Amount"], jArray[1]["Amount"], jArray[0]["Amount"] ];
     const barColors = [ "#03045e", "#0077b6", "#00b4d8", "#90e0ef", "#b5d1e2" , "#caf0f8" ];
 
     new Chart("restaurantDashbaordChart", {
--- src/services/owner/DashboardService.php
+++ src/services/owner/DashboardService.php
@@ -9,20 +9,65 @@
  * Data utworzenia: 2023-01-02, 22:32:06                       *
  * Autor: Miłosz Gilga                                         *
  *                                                             *
- * Ostatnia modyfikacja: 2023-01-02 22:35:29                   *
- * Modyfikowany przez: Miłosz Gilga                            *
+ * Ostatnia modyfikacja: 2023-01-11 21:12:27                   *
+ * Modyfikowany przez: patrick012016                           *
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 namespace App\Owner\Services;
 
+use PDO;
+use Exception;
+use App\Core\Config;
 use App\Core\MvcService;
- 
+
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 class DashboardService extends MvcService
 {
+    private $_banner_message = '';
+    private $_banner_error = false;
+
     protected function __construct()
     {
         parent::__construct();
+    }
+
+    /**
+     * Metoda zwracająca dane do pliku js aby wygenerować wykres w głównym widoku panelu restauratora.
+     */
+    public function graph()
+    {
+        $result = array();
+        for ($i = 0; $i < 6; $i++) 
+        {
+            $date = strtotime("-" . $i . " day", time());
+            $time = date("Y-m-d", $date);
+            try {
+                $this->dbh->beginTransaction();
+                $query = "
+                    SELECT :date AS day, count(*) AS number FROM orders WHERE DATE(date_order) = :date ORDER BY date_order DESC
+                ";
+                $statement = $this->dbh->prepare($query);
+                $statement->bindValue('date', $time, PDO::PARAM_STR);
+                $statement->execute();
+                $test = $statement->fetch(PDO::FETCH_ASSOC);
+                $first = array('Day' => $test['day'], 'Amount' => $test['number']);
+                array_push($result, $first);
+                $statement->closeCursor();
+                $this->dbh->commit();
+            } catch (Exception $e) {
+                $this->_banner_error = true;
+                $this->_banner_message = $e->getMessage();
+                $this->dbh->rollback();
+            }
+        }
+        ?>
+        <script type="text/javascript">var jArray =<?php echo json_encode($result); ?>;</script>
+        <script type="text/javascript" src="owner-charts.js"></script>
+        <?php
+        return array(
+            'banner_active' => !empty($this->_banner_message),
+            'banner_message' => $this->_banner_message,
+        );
     }
 }
