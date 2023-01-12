9be25a3fb105780ddf9f4819a57e4a415e617382
patrick012016 <patryk012016@gmail.com>
2023-01-12 21:11:18 +0100

    Fixed graphs in admin dashboard

--- public/static/js/admin-charts.js
+++ public/static/js/admin-charts.js
@@ -7,7 +7,7 @@
  * Data utworzenia: 2023-01-06, 04:16:09                       *
  * Autor: Miłosz Gilga                                         *
  *                                                             *
- * Ostatnia modyfikacja: 2023-01-12 03:20:30                   *
+ * Ostatnia modyfikacja: 2023-01-12 21:09:38                   *
  * Modyfikowany przez: patrick012016                           *
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
@@ -45,10 +45,42 @@ function generateAdminCharData() {
     });
 }
 
+function generateAdminCharv2Data() {
+    var barColors = ['#03045e', '#0077b6', '#00b4d8', '#90e0ef', '#b5d1e2' , '#caf0f8', '#27ffef', '#03045e', '#0077b6', '#00b4d8',];
+    const baseDir = document.getElementById('server-dir');
+    $.ajax({
+        url: baseDir.dataset.contentDir + 'admin/dashboard/graph',
+        type: 'GET',
+        dataType: 'json',
+        success: function(res) {      
+            new Chart('adminDashbaordv2Chart', {
+                type: 'bar',
+                data: {
+                    labels: res['coupons'].map(v => v.Name),
+                    datasets: [
+                        {
+                            backgroundColor: barColors,
+                            data: res['coupons'].map(v => v.Uses)
+                        },
+                    ],
+                },
+                options: {
+                    legend: { display: false },
+                    title: { display: true, fontSize: 18, text: 'Dane statystyczne użyć poszczególnych kuponów wszystkich restauracji' },
+                    responsive: true,
+                    maintainAspectRatio: false,
+                },
+            });
+            
+        }
+    });
+}
+
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 function onLoad() {
     generateAdminCharData();
+    generateAdminCharv2Data();
 };
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
