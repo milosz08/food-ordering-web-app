78a7bb24c44d74b580293aede842921a5860ff4a
patrick012016 <patryk012016@gmail.com>
2023-01-16 18:09:18 +0100

    Added display of information data in the restaurateur profile

--- src/services/owner/ProfileService.php
+++ src/services/owner/ProfileService.php
@@ -9,16 +9,22 @@
  * Data utworzenia: 2023-01-02, 22:32:11                       *
  * Autor: Miłosz Gilga                                         *
  *                                                             *
- * Ostatnia modyfikacja: 2023-01-16 14:38:49                   *
- * Modyfikowany przez: Miłosz Gilga                            *
+ * Ostatnia modyfikacja: 2023-01-16 18:06:53                   *
+ * Modyfikowany przez: patrick012016                           *
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 namespace App\Owner\Services;
 
+use PDO;
+use Exception;
 use App\Core\MvcService;
 use App\Core\ResourceLoader;
+use App\Models\OwnerProfileModel;
+use App\Services\Helpers\SessionHelper;
+
 
 ResourceLoader::load_service_helper('SessionHelper');
+ResourceLoader::load_model('OwnerProfileModel', 'restaurant');
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -35,4 +41,39 @@ class ProfileService extends MvcService
     }
 
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    /**
+     * Metoda odpowiadająca za wyświetlanie danych o użytkowniku w profilu.
+     */
+    public function profile()
+    {
+        $owner_profile = new OwnerProfileModel;
+        try
+        {
+            $this->dbh->beginTransaction();
+
+            // zapytanie do bazy danych, które zwróci informacje kontaktowe właściciela
+            $query = "
+                SELECT u.first_name, u.last_name, u.login, u.email, u.phone_number, IFNULL(u.photo_url, 'static/images/default-profile-image.jpg') AS photo_url,
+                CONCAT('ul. ', ua.street, ' ', ua.building_nr, IF(ua.locale_nr IS NOT NULL, (CONCAT('/',ua.locale_nr)), ('')) , ', ', 
+                ua.post_code, ' ', ua.city) AS address FROM users u INNER JOIN user_address ua ON u.id = ua.user_id WHERE u.id = :id 
+            ";
+            $statement = $this->dbh->prepare($query);
+            $statement->bindValue('id', $_SESSION['logged_user']['user_id']);
+            $statement->execute();
+
+            $owner_profile = $statement->fetchObject(OwnerProfileModel::class);
+
+            $statement->closeCursor();
+            if ($this->dbh->inTransaction()) $this->dbh->commit();
+        }
+        catch (Exception $e)
+        {
+            $this->dbh->rollback();
+            SessionHelper::create_session_banner(SessionHelper::OWNER_PROFILE_PAGE_BANNER, $e->getMessage(), true);
+        }
+        return array(
+            'profile' => $owner_profile,
+        );
+    }
 }
